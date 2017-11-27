8e813b01b4d4c1ed61bf1b8f9a8f158c8d1f8f13
VPA <rda@narola.email>
2017-11-27 14:58:36 +0530

    home

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -34,10 +34,9 @@ class Home extends CI_Controller
         if($res_post_data['post_type']  == 'gallery'){
             $all_posts = $this->db->get_where('gallery',['post_id'=>$res_post_data['id']])->result_array();
         }
+        
         // pr($all_posts);
         // pr($res_post_data,1);
-
-
 
         if (!empty($sess_data))
         {
