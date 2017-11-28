b2e2fdad12a21cdf92c572e808f3a022b10bd895
VPA <rda@narola.email>
2017-11-28 09:49:59 +0530

    minor fixes

--- application/views/front/layouts/layout_main.php
+++ application/views/front/layouts/layout_main.php
@@ -3,12 +3,11 @@
     <?php $this->load->view('front/layouts/layout_header'); ?>    
     <?php $this->load->view($subview); ?>
     <?php $this->load->view('front/layouts/side_bar'); ?>
-	
-	
-	
+    
 	<script type="text/javascript" src="<?php echo DEFAULT_JS_PATH.'custom.js'; ?>"></script>
-
+	<!-- Go to www.addthis.com/dashboard to customize your tools --> 
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5948f4fb5a7fea54"></script> 
 </body>
+    <?php $this->load->view('front/layouts/layout_modal'); ?>
 
-<!-- Go to www.addthis.com/dashboard to customize your tools --> <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5948f4fb5a7fea54"></script> 
 </html>
---
+++ application/views/front/layouts/layout_modal.php
@@ -0,0 +1,70 @@
+<div class="modal fade" id="login-register" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-body">
+            <div class="sing_in_up"> 
+                <div class="sign-in hide">
+                    <div class="sign-in-l">
+                        <h2>
+                            <big>Sign in</big>
+                            <small>Please enter your login information.</small>
+                        </h2>
+                        <form>
+                            <input type="text" name="" class="input-css" placeholder="Name"/>
+                            <input type="text" name="" class="input-css" placeholder="E-mail"/>
+                            <div class="remember-forgat">
+                                <div class="checkbox">
+                                    <input id="check1" type="checkbox" name="check" value="check1">
+                                    <label for="check1">Remember Me</label>
+                                </div>  
+                                <a class="cursor_pointer">
+                                    Forget password?
+                                </a>
+                            </div>
+                            <button type="submit" name="">SIGN IN</button>
+                        </form>
+                    </div>
+                    <div class="sign-in-r">
+                        <h3>Don’t have an account?</h3>
+                        <p>When an unknown printer took a galley of type and scrambled it to make a type specimen book has survived not only five.</p>
+                        <a href="javascript:void(0)" class="btn_sign_up">sign up</a>
+                    </div>
+                </div>
+                <div class="sign-up">
+                    <div class="sign-in-l">
+                        <h2>
+                            <big>Sign Up</big>
+                            <small>Please enter your login information.</small>
+                        </h2>
+                        <form>
+                            <input type="text" name="" class="input-css" placeholder="Name"/>
+                            <input type="text" name="" class="input-css" placeholder="E-mail"/>
+                            <input type="password" name="" class="input-css" placeholder="Password"/>
+                            <input type="password" name="" class="input-css" placeholder="Re-Password"/>
+                            <div class="remember-forgat">
+                                <div class="checkbox">
+                                    <input id="check2" type="checkbox" name="check" value="check2">
+                                    <label for="check2">Term & Condition</label>
+                                </div>
+                            </div>
+                            <button type="submit" name="">SIGN IN</button>
+                        </form>
+                    </div>
+                    <div class="sign-in-r">
+                        <h3>Have an account?</h3>
+                        <p>When an unknown printer took a galley of type and scrambled it to make a type specimen book has survived not only five.</p>
+                        <a href="javascript:void(0)" class="btn_sign_in">sign IN</a>
+                    </div>
+                </div>
+            </div>
+            <div class="re-password hide">
+                <h2>Forgot password</h2>
+                <p>Please enter your email information.</p>
+                <form>
+                    <input type="text" name="" placeholder="E-mail"/>
+                    <button type="submit">Submit</button>
+                    <button type="reset">cencel</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
