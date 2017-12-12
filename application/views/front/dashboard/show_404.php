533a40e8a59afae3286c79c78b4c025ef8f2695f
chirag <cm@narola.email>
2017-12-12 17:39:52 +0530

    nik minor fixes

--- application/views/front/dashboard/show_404.php
+++ application/views/front/dashboard/show_404.php
@@ -1,4 +1,3 @@
-<div class="white-box">
-    <h3 class="h3-title"> Page Not Found</h3>
-    <img src="<?php echo base_url().'public/front/images/404.png'; ?>" style="width: 100%" alt=""/>
+<div class="white-box page-404">
+    <img src="<?php echo base_url().'public/front/images/404.png'; ?>" style="max-width: 100%" alt=""/>
 </div>
